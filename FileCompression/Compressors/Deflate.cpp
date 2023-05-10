77c9d7a447af1e4f8094b3746681cadaad6b19c1
Mathieu BREBION <mathieu.brebion@epita.fr>
2023-05-11 00:27:27 +0200

    Started uncompressed block encoding implementation

---
+++ FileCompression/Compressors/Deflate.cpp
@@ -0,0 +1,12 @@
+﻿#include "Deflate.h"
+
+std::tuple<char*, int> Deflate::compress(const char* data, int size)
+{
+    char* res = new char[size + 1];
+    res[0] = data[0] >> 3;
+    for (int i = 1; i < size; i++)
+        res[i] = (data[i - 1] & 7) << 5 | data[i] >> 3;
+    res[size] = (data[size - 1] & 7) >> 5;  
+
+    return {res, size + 1};
+}
---
+++ FileCompression/Compressors/Deflate.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include <tuple>
+
+class Deflate
+{
+public:
+    static std::tuple<char*, int> compress(const char* data, int size);
+};
