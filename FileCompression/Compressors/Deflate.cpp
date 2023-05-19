75e3ebc8a7643359ab2368bf38937510e92162cd
Mathieu BREBION <mathieu.brebion@epita.fr>
2023-05-19 23:36:25 +0200

    Implemented stored data decompression

--- FileCompression/Compressors/Deflate.cpp
+++ FileCompression/Compressors/Deflate.cpp
@@ -1,12 +1,103 @@
 ﻿#include "Deflate.h"
 
-std::tuple<char*, int> Deflate::compress(const char* data, int size)
+#include <exception>
+#include <iostream>
+#include <vector>
+
+std::tuple<bool, std::tuple<char*, int>> Deflate::decompress_block(StreamReader& reader)
+{
+    std::tuple<char*, int> decompressed_block;
+    const bool is_final_block = reader.read_bits(1)[0];
+    
+    switch (reader.read_number(2))
+    {
+        case 0:
+            decompressed_block = get_stored_data(reader);
+            break;
+        case 1:
+            decompressed_block = get_fixed_huffman_data(reader);
+            break;
+        case 2:
+            decompressed_block = get_dynamic_huffman_data(reader);
+            break;
+        case 3:
+            throw std::exception("Reserved block type!");
+        default: // Should never happen
+            throw std::exception("Unknown block type!");
+    }
+
+
+    return {is_final_block, decompressed_block};
+}
+
+std::tuple<char*, int> Deflate::get_stored_data(StreamReader& reader)
+{
+    reader.skip_end_of_byte();
+    const auto len = static_cast<short>(reader.read_number(16));
+    const auto nlen = static_cast<short>(reader.read_number(16));
+    if (len != ~nlen)
+        throw std::exception("Invalid stored block length!");
+    char* bytes = reader.read_bytes(len);
+
+    return {bytes, len};
+}
+
+std::tuple<char*, int> Deflate::get_fixed_huffman_data(StreamReader& reader)
+{
+    throw std::exception("Not implemented yet!");
+}
+
+std::tuple<char*, int> Deflate::get_dynamic_huffman_data(StreamReader& reader)
+{
+    throw std::exception("Not implemented yet!");
+}
+
+std::tuple<char*, int> Deflate::compress(const unsigned char* data, const int size)
 {
     char* res = new char[size + 1];
-    res[0] = data[0] >> 3;
-    for (int i = 1; i < size; i++)
-        res[i] = (data[i - 1] & 7) << 5 | data[i] >> 3;
-    res[size] = (data[size - 1] & 7) >> 5;  
+    res[0] = 1; // Last block, no compression
+    for (int i = 0; i < size; i++)
+        res[i + 1] = data[i];
 
     return {res, size + 1};
 }
+
+std::tuple<char*, int> Deflate::decompress(const unsigned char* data, const int offset)
+{
+    try
+    {
+        int off = offset;
+        std::vector<std::tuple<char*, int>> decompressed_blocks;
+        StreamReader reader(data, offset);
+        std::tuple<bool, std::tuple<char*, int>> decompression;
+        do
+        {
+            decompression = decompress_block(reader);
+            decompressed_blocks.push_back(std::get<1>(decompression));
+            off += std::get<1>(std::get<1>(decompression));
+        }
+        while (!std::get<0>(decompression));
+        
+        int decompressed_size = 0;
+        for (auto decompressed_block : decompressed_blocks)
+            decompressed_size += std::get<1>(decompressed_block);
+
+        char* uncompressed_data = new char[decompressed_size];
+
+        off = 0;
+        for (auto decompressed_block : decompressed_blocks)
+        {
+            for (int i = 0; i < std::get<1>(decompressed_block); i++)
+                uncompressed_data[off++] = std::get<0>(decompressed_block)[i];
+        }
+
+        return {uncompressed_data, decompressed_size};
+    }
+    catch (std::exception& e)
+    {
+        std::cout << e.what();
+        throw;
+    }
+}
+    
+    
--- FileCompression/Compressors/Deflate.h
+++ FileCompression/Compressors/Deflate.h
@@ -1,8 +1,101 @@
 ﻿#pragma once
 #include <tuple>
+#include <vector>
 
 class Deflate
 {
+    /**
+     * \brief Reader for compressed DEFLATE data.
+     */
+    class StreamReader
+    {
+        unsigned byte_offset_;
+        unsigned bit_index_ = 0;
+        const unsigned char* data_;
+    public:
+        StreamReader(const unsigned char* data, const unsigned offset) : byte_offset_{ offset }, data_ { data } {}
+
+        /**
+         * \brief Moves the reader to the next byte
+         */
+        void skip_end_of_byte()
+        {
+            if (bit_index_ != 0)
+            {
+                bit_index_ = 0;
+                ++byte_offset_;
+            }
+        }
+
+        /**
+         * \brief Reads a number of bits from the data in reverse order (first bit read is the LSB of the first byte)
+         * \param count Number of bits to read
+         */
+        std::vector<bool> read_bytes_forward(const int count)
+        {
+            std::vector<bool> bits = read_bits(count);
+            std::reverse(bits.begin(), bits.end());
+
+            return bits;
+        }
+
+        /**
+         * \brief Reads a number of bits from the data in reverse order (first bit read is the LSB of the first byte)
+         * \param count Number of bits to read
+         */
+        std::vector<bool> read_bits(const int count)
+        {
+            std::vector<bool> bits;
+            bits.reserve(count);
+
+            for (int i = 0; i < count; ++i)
+            {
+                bits.push_back(data_[byte_offset_] & (1 << bit_index_++));
+                if (bit_index_ == 8)
+                {
+                    bit_index_ = 0;
+                    ++byte_offset_;
+                }
+            }
+
+            return bits;
+        }
+
+        /**
+         * \brief Reads a number from the data
+         * \param bits Number of bits to read
+         */
+        int read_number(const int bits)
+        {
+            int n = 0;
+            for (int i = 0; i < bits; i++)
+            {
+                n += ((data_[byte_offset_] & (1 << bit_index_)) >> bit_index_++) << i;
+                if (bit_index_ == 8)
+                {
+                    bit_index_ = 0;
+                    ++byte_offset_;
+                }
+            }
+
+            return n;
+        }
+
+        char* read_bytes(const int count)
+        {
+            char* bytes = new char[count];
+            for (int i = 0; i < count; i++)
+                bytes[i] = data_[byte_offset_++];
+
+            return bytes;
+        }
+    };
+    
+    static std::tuple<bool, std::tuple<char*, int>> decompress_block(StreamReader& reader);
+    static std::tuple<char*, int> get_stored_data(StreamReader& reader);
+    static std::tuple<char*, int> get_fixed_huffman_data(StreamReader& reader);
+    static std::tuple<char*, int> get_dynamic_huffman_data(StreamReader& reader);
 public:
-    static std::tuple<char*, int> compress(const char* data, int size);
+    static std::tuple<char*, int> compress(const unsigned char* data, int size);
+    static std::tuple<char*, int> decompress(const unsigned char* data, int offset);
 };
