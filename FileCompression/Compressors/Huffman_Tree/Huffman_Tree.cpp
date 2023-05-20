2da8397e7e92ad7cde9f6375cf58baf31278b31f
Mathieu BREBION <mathieu.brebion@epita.fr>
2023-05-20 13:33:24 +0200

    Added construction with hierarchical notation and key reading with custom DEFLATE stream reader

--- FileCompression/Compressors/Huffman_Tree/Huffman_Tree.cpp
+++ FileCompression/Compressors/Huffman_Tree/Huffman_Tree.cpp
@@ -6,7 +6,7 @@
 
 void Huffman_Tree::generate_chars_of_bit_length(const Node& node, const int bit_length)
 {
-    if (node.val == 0)
+    if (node.key == 0)
     {
         if (node.left != nullptr)
             generate_chars_of_bit_length(*node.left, bit_length + 1);
@@ -19,6 +19,27 @@ void Huffman_Tree::generate_chars_of_bit_length(const Node& node, const int bit_
             chars_of_bit_length_[bit_length] = 1;
         else
             chars_of_bit_length_[bit_length]++;
+    }
+}
+
+void Huffman_Tree::add(Node* node, const char key, const int path, const int path_length)
+{
+    if(!path_length)
+    {
+        node->key = key;
+        return;
+    }
+    if(path & (1 << (path_length - 1)))
+    {
+        if(node->right == nullptr)
+        node->right = new Node(0, nullptr, nullptr);
+        add(node->right, key, path, path_length - 1);        
+    }
+    else
+    {
+        if(node->left == nullptr)
+            node->left = new Node(0, nullptr, nullptr);
+        add(node->left, key, path, path_length - 1);
     }
 }
 
@@ -41,6 +62,25 @@ Huffman_Tree::Huffman_Tree(const std::map<char, int>& frequency_table)
 
     root_ = l.begin()->second;
     generate_chars_of_bit_length(*root_, 0);
+}
+
+Huffman_Tree::Huffman_Tree(const std::map<char, std::pair<int, int>>& tree)
+{
+    root_ = new Node(0, nullptr, nullptr);
+
+    for (auto p : tree)
+        add(root_, std::get<0>(p), std::get<0>(std::get<1>(p)), std::get<1>(std::get<1>(p)));
+
+    generate_chars_of_bit_length(*root_, 0);
+}
+
+int Huffman_Tree::read_key(Deflate::Stream_Reader& reader) const
+{
+    const Node* current = root_;
+    while(current->key == 0)
+        current = reader.read_bit() ? current->right : current->left;
+
+    return current->key;
 }
 
 std::ostream& operator<<(std::ostream& os, const Huffman_Tree& huffman_tree)
--- FileCompression/Compressors/Huffman_Tree/Huffman_Tree.h
+++ FileCompression/Compressors/Huffman_Tree/Huffman_Tree.h
@@ -3,22 +3,24 @@
 #include <ostream>
 #include <vector>
 
+#include "../Deflate.h"
+
 class Huffman_Tree
 {
     class Node
     {
     public:
-        char val;
+        char key;
         Node* left;
         Node* right;
-        Node(const char val, Node* left, Node* right) : val(val), left(left), right(right) {}
+        Node(const char val, Node* left, Node* right) : key(val), left(left), right(right) {}
         ~Node(){delete left; delete right;}
 
         friend std::ostream& operator<<(std::ostream& os, const Node* node)
         {
             if(node == nullptr)
-                return os << 'null';
-            os << node->val << " l:" << node->left << " r:" << node->right;
+                return os << "null";
+            os << node->key << " l:" << node->left << " r:" << node->right;
 
             return os;
         }
@@ -26,11 +28,13 @@ class Huffman_Tree
     
     std::map<int, int> chars_of_bit_length_;
     void generate_chars_of_bit_length(const Node& node, int bit_length);
-    
+    static void add(Node* node, char key, int path, int path_length);
     
     Node* root_;
 public:
     explicit Huffman_Tree(const std::map<char, int>& frequency_table);
+    explicit Huffman_Tree(const std::map<char, std::pair<int, int>>& tree);
     ~Huffman_Tree(){delete root_;}
+    int read_key(Deflate::Stream_Reader& reader) const;
     friend std::ostream& operator<<(std::ostream& os, const Huffman_Tree& huffman_tree);
 };
